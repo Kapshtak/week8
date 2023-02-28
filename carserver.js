6e81adab6cfe15638daed91ea8060809f966ba45
Arseniiy Kapshtyk <kapshtykas@yandex.ru>
2023-02-28 16:48:36 +0200

    almost working prototype

--- carserver.js
+++ carserver.js
@@ -1,9 +1,44 @@
 'use strict'
-
+const carsLibrary = require('./cars.json')
 const http = require('http')
 const port = 8000
 const host = 'localhost'
-const { Car, writeCar, findCar } = require('./cars.js')
+const fs = require('fs')
+
+const filePath = 'cars.json'
+
+class Car {
+  constructor(licence, maker, model, owner, price, color) {
+    this.licence = licence
+    this.maker = maker
+    this.model = model
+    this.owner = owner
+    this.price = price
+    this.color = color
+  }
+  getCar() {
+    return {
+      licence: this.licence,
+      maker: this.maker,
+      model: this.model,
+      owner: this.owner,
+      price: this.price,
+      color: this.color
+    }
+  }
+}
+
+function writeCar(obj) {
+  carsLibrary.push(obj)
+  const testJsonString = JSON.stringify(carsLibrary)
+  fs.writeFile(filePath, testJsonString, (err) => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('File written successfully')
+    }
+  })
+}
 
 const server = http.createServer((request, response) => {
   const { pathname, searchParams } = new URL(
@@ -21,10 +56,35 @@ const server = http.createServer((request, response) => {
     const carObj = car.getCar()
     writeCar(carObj)
     sendJSON(response, carObj)
-  } else if (route === '/get-car') {
+  } else if (route === '/get-cars') {
+    fs.readFile(filePath, 'utf8', (err, contents) => {
+      if (err) {
+        sendJSON(response, { error: 'Not found' }, 404)
+      }
+      try {
+        const jsonString = JSON.parse(contents)
+        sendJSON(response, jsonString, 200)
+      } catch (jsonError) {
+        console.error('Error parsing JSON')
+      }
+    })
+  } else if (route === '/find-car') {
     const licence = searchParams.get('licence')
-    const data = findCar(licence)
-    sendJSON(response, data)
+    fs.readFile(filePath, 'utf8', (err, contents) => {
+      if (err) {
+        sendJSON(response, { error: 'Not found' }, 404)
+      }
+      try {
+        const jsonString = JSON.parse(contents)
+        for (const car of jsonString) {
+          if (car.licence == licence) {
+            sendJSON(response, car, 200)
+          } 
+        }
+      } catch (jsonError) {
+        console.error('Error parsing JSON')
+      }
+    })
   } else {
     sendJSON(response, { error: 'Not found' }, 404)
   }
---
+++ main.js
@@ -0,0 +1,55 @@
+const licence = document.getElementById('licence')
+const maker = document.getElementById('maker')
+const model = document.getElementById('model')
+const owner = document.getElementById('owner')
+const price = document.getElementById('price')
+const color = document.getElementById('color')
+const table = document.getElementById('all-cars')
+const searchField = document.getElementById('search')
+const submitButton = document.getElementById('submit')
+const resetButton = document.getElementById('reset')
+const searchButton = document.getElementById('find')
+const result = document.getElementById('result')
+
+async function createCar() {
+  const url = `http://localhost:8000/create-car?licence=${licence.value}&maker=${maker.value}&model=${model.value}&owner=${owner.value}&price=${price.value}&color=${color.value}`
+  const xmlHttp = new XMLHttpRequest()
+  xmlHttp.open('GET', url, false)
+  xmlHttp.send(null)
+  return xmlHttp.res
+}
+
+async function findCar() {
+  const data = await fetch(`http://localhost:8000/find-car?licence=${searchField.value}`, { mode: 'cors' })
+  const jsonData = await data.json()
+  result.textContent = `Licence plate: ${jsonData.licence}, Manufacturer: ${jsonData.maker}, Model: ${jsonData.model}, Owner: ${jsonData.owner}, Price: ${jsonData.price}, Color: ${jsonData.color}` 
+  jsonData.color
+}
+
+function createCell(text) {
+  const td = document.createElement('td')
+  td.textContent = text
+  return td
+}
+
+async function getAllCars() {
+  const data = await fetch('http://localhost:8000/get-cars', { mode: 'cors' })
+  const jsonData = await data.json()
+  for (const car of jsonData) {
+    const tr = document.createElement('tr')
+    tr.appendChild(createCell(car.licence))
+    tr.appendChild(createCell(car.maker))
+    tr.appendChild(createCell(car.model))
+    tr.appendChild(createCell(car.owner))
+    tr.appendChild(createCell(car.price))
+    tr.appendChild(createCell(car.color))
+    table.appendChild(tr)
+  }
+}
+
+getAllCars()
+searchButton.addEventListener('click', findCar)
+submitButton.addEventListener('click', createCar)
+resetButton.addEventListener('click', function() {
+  window.location.reload()
+})
